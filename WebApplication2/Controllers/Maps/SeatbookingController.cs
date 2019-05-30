21ec73da7cb95089354a117fb99232faf36640f5
manojchand <manojchand@gmail.com>
2019-05-30 19:44:16 +0530

    Add project files.

---
+++ WebApplication2/Controllers/Maps/SeatbookingController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using WebApplication2;
+
+namespace EJ2MVCSampleBrowser.Controllers.Maps
+{
+    public partial class MapsController : Controller
+    {
+        private TestEntities db = new TestEntities();
+        // GET: Seatbooking
+        public ActionResult Seatbooking()
+        {
+            //use from file for development
+            //ViewBag.shapeData = this.SeatData();
+            ViewBag.shapeData = SeatDataFromDatabase();
+            return View();
+        }
+        public object SeatData()
+        {
+            string allText = System.IO.File.ReadAllText(Server.MapPath("~/App_Data/MapData/Seat.json"));
+            return JsonConvert.DeserializeObject(allText, typeof(object));
+        }
+
+        public object SeatDataFromDatabase()
+        {
+            var v= db.SeatSelections.ToList();
+            System.Text.StringBuilder builder = new System.Text.StringBuilder("");
+            builder.Append("{\'type\': \'FeatureCollection\', \'features\': [ \r\n ");
+
+            foreach (var eachItem in v)
+            {
+                builder.Append("{ \'type\': \'Feature\', \'geometry\': { \'type\': \'MultiPolygon\', \'coordinates\': ");
+                builder.Append("[[[[");
+
+                string s = eachItem.seat.AsText();
+                int count = 0;
+                string[] numbers = Regex.Split(s, @"\D+");
+                foreach (string value in numbers)
+                {
+                    if (!string.IsNullOrEmpty(value))
+                    {
+                        int i = int.Parse(value);
+
+                        if (count == 1)
+                        {
+                            builder.Append(i);
+                        }
+                        else if (count == 20)
+                        {
+                            builder.Append(", ");
+                            builder.Append(i);
+                        }
+                        else if (count == 11)
+                        {
+                            
+                            builder.Append("]]], [[[");
+                            builder.Append(i);
+                        }
+                        else 
+                        {
+                            if (count % 2 == 0)
+                            {
+                                builder.Append(", ");
+                                builder.Append(i);
+                            }
+                            else
+                            {
+                                builder.Append("], [");
+                                builder.Append(i);
+                            }
+                        }   
+                    }
+                    count++;
+                }
+                builder.Append("]]]] ");
+                builder.Append("}, \'properties\': { \'seatno\': " + eachItem.seatno + ", \'fill\': \'"+ eachItem.fill + "\' } }, \r\n");
+            }
+            builder.Remove(builder.Length - 4, 4);
+            builder.Append("] }");
+            builder.Replace("'", "\"");
+            string allText =  builder.ToString();
+            return JsonConvert.DeserializeObject(allText.ToString(), typeof(object));
+        }
+    }
+}
---
+++ WebApplication2/Controllers/SeatSelectionsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2;
+
+namespace WebApplication2.Controllers
+{
+    public class SeatSelectionsController : Controller
+    {
+        private TestEntities db = new TestEntities();
+
+        // GET: SeatSelections
+        public ActionResult Index()
+        {
+            return View(db.SeatSelections.ToList());
+        }
+
+        // GET: SeatSelections/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SeatSelection seatSelection = db.SeatSelections.Find(id);
+            if (seatSelection == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seatSelection);
+        }
+
+        // GET: SeatSelections/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SeatSelections/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,seat,fill,seatno")] SeatSelection seatSelection)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SeatSelections.Add(seatSelection);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(seatSelection);
+        }
+
+        // GET: SeatSelections/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SeatSelection seatSelection = db.SeatSelections.Find(id);
+            if (seatSelection == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seatSelection);
+        }
+
+        // POST: SeatSelections/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,seat,fill,seatno")] SeatSelection seatSelection)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(seatSelection).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(seatSelection);
+        }
+
+        // GET: SeatSelections/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SeatSelection seatSelection = db.SeatSelections.Find(id);
+            if (seatSelection == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seatSelection);
+        }
+
+        // POST: SeatSelections/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SeatSelection seatSelection = db.SeatSelections.Find(id);
+            db.SeatSelections.Remove(seatSelection);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
